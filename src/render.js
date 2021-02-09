673f1406597891e775643dbc9992fa20ea8648ff
Bruno Foggia <bruno.foggia@cbyk.com.br>
2021-02-09 13:52:32 -0300

    added vx on render serializedata

--- src/render.js
+++ src/render.js
@@ -1,10 +1,18 @@
-import _ from 'underscore-99xp';
-import vx from 'backbone-front-99xp';
-import Mn from 'backbone.marionette';
-import utils from './utils';
+import _ from "underscore-99xp";
+import vx from "backbone-front-99xp";
+import Mn from "backbone.marionette";
+import utils from "./utils";
 
-var obj = {model: {}, collection: {}, view: {}, app: {}, model_prototype: {}, collection_prototype: {}, view_prototype: {}, app_prototype: {}};
-
+var obj = {
+    model: {},
+    collection: {},
+    view: {},
+    app: {},
+    model_prototype: {},
+    collection_prototype: {},
+    view_prototype: {},
+    app_prototype: {},
+};
 
 obj.view_prototype._render = Mn.View.prototype.render;
 
@@ -14,37 +22,44 @@ obj.view_prototype.renderTimer = null;
 obj.view_prototype._firstRender = true;
 
 obj.view_prototype.renderSync = function () {
-    this.addLoading && this.addLoading('', 'renderSync');
-    var isReadyFn = _.bind((this.isReady ? this.isReady : utils.isReady), this),
+    this.addLoading && this.addLoading("", "renderSync");
+    var isReadyFn = _.bind(this.isReady ? this.isReady : utils.isReady, this),
         isReady = isReadyFn();
 
-    clearTimeout(this.renderTimer); this.renderTimer=null;
-    if (isReady===true) {
-        this.removeLoading && this.removeLoading('renderSync', 0);
+    clearTimeout(this.renderTimer);
+    this.renderTimer = null;
+    if (isReady === true) {
+        this.removeLoading && this.removeLoading("renderSync", 0);
         this.renderStatus = true;
         // _.bind(obj.view_prototype._render, this)();
         this._render();
-    } else if(this.renderTimer===null) {
+    } else if (this.renderTimer === null) {
         this.renderTimer = setTimeout(() => {
-            vx.debug.log(`interval insure ${isReady[0]} -${isReady[1] || '-'}`);
+            vx.debug.log(`interval insure ${isReady[0]} -${isReady[1] || "-"}`);
             this.render();
         }, 500);
     }
     return this;
 };
 
-
 obj.view_prototype.templates = {};
 obj.view_prototype.renderPartial = function (viewName, opts = {}) {
     return this.templates[viewName](_.extend(this.serializeData(), opts));
-}
+};
 
 obj.view_prototype.serializeData = function () {
-    var App = vx.locate('iApp');
+    var App = vx.locate("iApp");
     return {
-        App, _, view: this, model: this.model, collection: this.collection, 
-        relatedLists: this.relatedLists || {}, options: this.options, cid: this.cid, 
-        renderPartial: (viewName, opts) => this.renderPartial(viewName, opts)
+        App,
+        vx,
+        _,
+        view: this,
+        model: this.model,
+        collection: this.collection,
+        relatedLists: this.relatedLists || {},
+        options: this.options,
+        cid: this.cid,
+        renderPartial: (viewName, opts) => this.renderPartial(viewName, opts),
     };
 };
 
